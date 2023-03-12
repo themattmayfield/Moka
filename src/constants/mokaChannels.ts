d7343866c9a65046198e533b145206c2df30b29f
Matthew Mayfield <mattmayf411@gmail.com>
2023-03-12 13:52:52 -0500

    update channel

--- src/constants/mokaChannels.ts
+++ src/constants/mokaChannels.ts
@@ -1,6 +1,7 @@
 const channels = [
-  { general: "894626621405921280" },
-  { test: "927249226310750268" },
+  // { general: "894626621405921280" },
+  // { test: "927249226310750268" },
+  { publicBot: "894624100981555251" },
 ];
 
 const MOKA_CHANNELS = channels.map((channel) => Object.values(channel)[0]);
---
+++ src/server.ts
@@ -0,0 +1,109 @@
+import express from "express";
+import http from "http";
+import { config } from "./config/config";
+// import Logging from "./lib/Logging";
+
+const router = express();
+
+/** Log the request */
+router.use(
+  (
+    req: { method: any; url: any; socket: { remoteAddress: any } },
+    res: { on: (arg0: string, arg1: () => void) => void; statusCode: any },
+    next: () => void
+  ) => {
+    /** Log the req */
+    console.log(
+      `Incomming - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`
+    );
+
+    res.on("finish", () => {
+      /** Log the res */
+      console.log(
+        `Result - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}] - STATUS: [${res.statusCode}]`
+      );
+    });
+
+    next();
+  }
+);
+
+router.use(express.urlencoded({ extended: true }));
+router.use(express.json());
+
+/** Rules of our API */
+router.use(
+  (
+    req: { method: string },
+    res: {
+      header: (arg0: string, arg1: string) => void;
+      status: (arg0: number) => {
+        (): any;
+        new (): any;
+        json: { (arg0: {}): any; new (): any };
+      };
+    },
+    next: () => void
+  ) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+
+    if (req.method == "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "PUT, POST, PATCH, DELETE, GET"
+      );
+      return res.status(200).json({});
+    }
+
+    next();
+  }
+);
+
+/** Healthcheck */
+router.get(
+  "/ping",
+  (
+    req: any,
+    res: {
+      status: (arg0: number) => {
+        (): any;
+        new (): any;
+        json: { (arg0: { hello: string }): any; new (): any };
+      };
+    },
+    next: any
+  ) => res.status(200).json({ hello: "world" })
+);
+
+/** Error handling */
+router.use(
+  (
+    req: any,
+    res: {
+      status: (arg0: number) => {
+        (): any;
+        new (): any;
+        json: { (arg0: { message: string }): void; new (): any };
+      };
+    },
+    next: any
+  ) => {
+    const error = new Error("Not found");
+
+    console.log(error);
+
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+);
+
+http
+  .createServer(router)
+  .listen(config.server.port, () =>
+    console.log(`Server is running on port ${config.server.port}`)
+  );
