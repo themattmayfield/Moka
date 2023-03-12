e65a43ff0aec1ee8c19081b3082904ca93983134
Matthew Mayfield <mattmayf411@gmail.com>
2023-03-12 11:52:36 -0500

    update

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,112 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import http from "http";
+import { config } from "./config/config";
+import Logging from "./lib/Logging";
 
-const PORT = process.env.PORT || 5000;
+const router = express();
 
-const app = express();
+/** Log the request */
+router.use(
+  (
+    req: { method: any; url: any; socket: { remoteAddress: any } },
+    res: { on: (arg0: string, arg1: () => void) => void; statusCode: any },
+    next: () => void
+  ) => {
+    /** Log the req */
+    Logging.info(
+      `Incomming - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`
+    );
 
-//////////////////////////////////////////////////////////////////
-//             EXPRESS SERVER SETUP FOR UPTIME ROBOT            //
-//////////////////////////////////////////////////////////////////
-app.use(express.urlencoded({ extended: true }));
+    res.on("finish", () => {
+      /** Log the res */
+      Logging.info(
+        `Result - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}] - STATUS: [${res.statusCode}]`
+      );
+    });
 
-app.use("/", (request: Request, response: Response) => {
-  response.sendStatus(200);
-});
-app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
+    next();
+  }
+);
 
-//////////////////////////////////////////////////////////////////
-//                    DISCORD CLIENT LISTENERS                  //
-//////////////////////////////////////////////////////////////////
-// Discord Events: https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=e-channelCreate
+router.use(express.urlencoded({ extended: true }));
+router.use(express.json());
+
+/** Rules of our API */
+router.use(
+  (
+    req: { method: string },
+    res: {
+      header: (arg0: string, arg1: string) => void;
+      status: (arg0: number) => {
+        (): any;
+        new (): any;
+        json: { (arg0: {}): any; new (): any };
+      };
+    },
+    next: () => void
+  ) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+
+    if (req.method == "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "PUT, POST, PATCH, DELETE, GET"
+      );
+      return res.status(200).json({});
+    }
+
+    next();
+  }
+);
+
+/** Healthcheck */
+router.get(
+  "/ping",
+  (
+    req: any,
+    res: {
+      status: (arg0: number) => {
+        (): any;
+        new (): any;
+        json: { (arg0: { hello: string }): any; new (): any };
+      };
+    },
+    next: any
+  ) => res.status(200).json({ hello: "world" })
+);
+
+/** Error handling */
+router.use(
+  (
+    req: any,
+    res: {
+      status: (arg0: number) => {
+        (): any;
+        new (): any;
+        json: { (arg0: { message: string }): void; new (): any };
+      };
+    },
+    next: any
+  ) => {
+    const error = new Error("Not found");
+
+    Logging.error(error);
+
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+);
+
+http
+  .createServer(router)
+  .listen(config.server.port, () =>
+    Logging.info(`Server is running on port ${config.server.port}`)
+  );
 
 //load environment variables
 import "./lib/variables";
--- src/server.ts
+++
@@ -1,5 +0,0 @@
-http
-  .createServer(router)
-  .listen(config.server.port, () =>
-    Logging.info(`Server running on port ${config.server.port}`)
-  );
